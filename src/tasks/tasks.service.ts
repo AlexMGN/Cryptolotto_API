46be1823b7900e7de8b34276818041a6d9658cfc
Mignon Alexandre <alex.mignon77@gmail.com>
2022-07-15 01:15:27 +0200

    Setup Cron Heroku

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,11 +5,11 @@ import { Cron } from '@nestjs/schedule';
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
 
-  @Cron('13 1 * * * *', {
+  @Cron('0 20 1 * * *', {
     name: 'create_lotteries',
     timeZone: 'Europe/Paris',
   })
   createLotteries() {
-    this.logger.debug('Il est 1h13 du matin');
+    this.logger.debug('Il est 1h20 du matin');
   }
 }
